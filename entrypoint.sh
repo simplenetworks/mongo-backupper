50fa5854a2fd3c01d287566cc0b43da8f18bde26
Marco Rosetti <marco.rosetti@teachat.it>
2020-03-20 08:42:33 +0100

    Better env variables handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,19 @@
 
 set -e
 
-CRON_SCHEDULE=${CRON_SCHEDULE:-0 1 * * *}
-export MONGO_HOST=${MONGO_HOST:-mongo}
-export MONGO_PORT=${MONGO_PORT:-27017}
-export FTP_HOST=${FTP_HOST:-ftp.simplevault.it}
-export FTP_USER=${FTP_USER:-2674000@aruba.it}
-export FTP_PASSWORD=${FTP_PASSWORD:-cdezxww2}
-export FTP_BACKUP_FOLDER=${FTP_BACKUP_FOLDER:-www.simplevault.it/fisioweb}
+CRON_SCHEDULE=${CRON_SCHEDULE}
 
-if [[ "$1" == 'no-cron' ]]; then
-    exec /backup.sh
-else
-    LOGFIFO='/var/log/cron.fifo'
-    if [[ ! -e "$LOGFIFO" ]]; then
-        mkfifo "$LOGFIFO"
-    fi
-    CRON_ENV="MONGO_HOST='$MONGO_HOST'"
-    CRON_ENV="$CRON_ENV\nMONGO_PORT='$MONGO_PORT'"
-    CRON_ENV="$CRON_ENV\nFTP_HOST='$FTP_HOST'"
-    CRON_ENV="$CRON_ENV\nFTP_USER='$FTP_USER'"
-    CRON_ENV="$CRON_ENV\nFTP_PASSWORD='$FTP_PASSWORD'"
-    CRON_ENV="$CRON_ENV\nFTP_BACKUP_FOLDER='$FTP_BACKUP_FOLDER'"
-    echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
-    crontab -l
-    cron
-    tail -f "$LOGFIFO"
+LOGFIFO='/var/log/cron.fifo'
+if [[ ! -e "$LOGFIFO" ]]; then
+    mkfifo "$LOGFIFO"
 fi
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' >/container.env
+echo -e "SHELL=/bin/bash\nBASH_ENV=/container.env" | crontab -
+LINE="$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1"
+(
+    crontab -l
+    echo -e "$LINE"
+) | crontab -
+crontab -l
+cron
+tail -f "$LOGFIFO"
